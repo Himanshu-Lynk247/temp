6566d3ab05d18e85695cf24f5ab5238228774351
Himanshu-Lynk247 <himanshu.tyagi@lynk247.com>
2026-01-20 15:57:11 +0530

    Update updaterCheck.bat

--- updaterCheck.bat
+++ updaterCheck.bat
@@ -4,7 +4,7 @@ REM Set the installation directory and other pathss
 SET "INSTALL_DIR=C:\Program Files\Lynk247"
 SET "EXE_NAME=Lynk247.exe"
 SET "JSON_NAME=version.json"
-
+SET "JAR_NAME=lynk247DeskApp.jar"
 SET "INSTALL_DIR_SRC=C:\Program Files\Lynk247\src\main\resources\com\optimize\lynk247DeskApp\view\stylesUi"
 SET "BREAK_STYLE_NAME=breakPage.css"
 SET "IDLE_DLL_NAME=CppDeskApp.dll"
@@ -14,7 +14,7 @@ SET "IDLE_DLL_URL=https://github.com/OptimizeInnnovations/Lynk247Updater/raw/ref
 SET "JSON_URL=https://raw.githubusercontent.com/Himanshu-Lynk247/temp/main/version.json"
 
 SET "EXE_URL=https://github.com/OptimizeInnnovations/Lynk247Updater/releases/download/LynkUpdate/Lynk247.exe"
-
+SET "JAR_URL=https://github.com/OptimizeInnnovations/Lynk247Updater/releases/download/LynkUpdate/lynk247DeskApp.jar"
 
 
 REM Step 1: Stop the running application
@@ -36,14 +36,15 @@ powershell -Command "Invoke-WebRequest '%BREAK_STYLE_URL%' -OutFile '%INSTALL_DI
 REM Download the EXE file using PowerShell's Invoke-WebRequest
 powershell -Command "Invoke-WebRequest '%EXE_URL%' -OutFile '%INSTALL_DIR%\%EXE_NAME%'"
 
+REM Download the JAR file using PowerShell's Invoke-WebRequest
+powershell -Command "Invoke-WebRequest '%JAR_URL%' -OutFile '%INSTALL_DIR%\%JAR_NAME%'"
 
 REM Download the JSON file using PowerShell's Invoke-WebRequest
 powershell -Command "Invoke-WebRequest '%JSON_URL%' -OutFile '%INSTALL_DIR%\%JSON_NAME%'"
 
 REM Step 4: Restart the application
 echo Restarting the application...
-explorer "%INSTALL_DIR%\%EXE_NAME%"
-
+start "" "%INSTALL_DIR%\%EXE_NAME%"
 
 REM Step 5: Exit the batch file
 exit
